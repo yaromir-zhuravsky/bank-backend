80b7c0dcc628d506c211868a100df27bb11a1d85
yaromir-zhuravsky <yaromir.zhuravsky@gmail.com>
2026-06-18 14:17:12 +0200

    4: Use rescuing for errors handling and move it to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+  def record_invalid(e)
+    render json: { errors: e.record.errors }, status: :unprocessable_entity
+  end
+
+  def record_not_found(e)
+    render json: { errors: "#{e.model} not found" }, status: :not_found
+  end
 end
--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -29,17 +29,12 @@ class OperationsController < ApplicationController
     operation_info = result.to_h[:operation]
     amount = operation_info[:amount]
 
-    account = Account.find_by(number: operation_info[:from])
-    return render status: :not_found, json: {errors: {account: "not found"}}unless account.present?
+    account = Account.find_by!(number: operation_info[:from])
 
     Operations::Withdraw.perform(account, amount)
 
     head :ok
-
-  rescue ActiveRecord::RecordInvalid => e
-    render json: { errors: e.record.errors }, status: :unprocessable_entity
   end
-
   def deposit
     result = DepositSchema.call(params.permit!.to_h)
     return render json: { errors: result.errors.to_h }, status: :unprocessable_entity unless result.success?
@@ -47,16 +42,13 @@ class OperationsController < ApplicationController
     operation_info = result.to_h[:operation]
     amount = operation_info[:amount]
 
-    account = Account.find_by(number: operation_info[:to])
-    return render status: :not_found, json: {errors: {account: "not found"}} unless account.present?
+    account = Account.find_by!(number: operation_info[:to])
 
     Operations::Deposit.perform(account, amount)
 
     head :ok
-
-  rescue ActiveRecord::RecordInvalid => e
-    render json: { errors: e.record.errors }, status: :unprocessable_entity
   end
+
 
   def transfer
     result = TransferSchema.call(params.permit!.to_h)
@@ -65,16 +57,11 @@ class OperationsController < ApplicationController
     operation_info = result.to_h[:operation]
     amount = operation_info[:amount]
 
-    sender_account = Account.find_by(number: operation_info[:from])
-    return render status: :not_found, json: {errors: {account: "not found"}} unless sender_account.present?
-    receiver_account = Account.find_by(number: operation_info[:to])
-    return render status: :not_found, json: {errors: {account: "not found"}} unless receiver_account.present?
+    sender_account = Account.find_by!(number: operation_info[:from])
+    receiver_account = Account.find_by!(number: operation_info[:to])
 
     Operations::Transfer.perform(sender_account, receiver_account, amount)
 
     head :ok
-
-  rescue ActiveRecord::RecordInvalid => e
-    render json: { errors: e.record.errors }, status: :unprocessable_entity
   end
 end
